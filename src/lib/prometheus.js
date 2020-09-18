70c186d151d3554c2f60e6d69617577bd7bd1c39
Mark Holtzhausen <mark.holtzhausen@superbalist.com>
2020-09-18 16:37:09 +0200

    Linting

--- src/lib/prometheus.js
+++ src/lib/prometheus.js
@@ -1,48 +1,51 @@
-'use strict';
-let express = require('express');
-let client = require('prom-client');
-let config = require('../config');
-client.collectDefaultMetrics({timeout: 30000});
+'use strict'
+
+const client = require('prom-client')
+const express = require('express')
+const logger = require('./logger')
+const {PROMETHEUS_PORT, PROMETHEUS_EXPORTER} = require('../config')
+
+client.collectDefaultMetrics({timeout: 30000})
 
 const receiveCount = new client.Counter({
-  name: 'pubsub_rest_proxy_receive_total',
-  help: 'Count of all messages recieved by pubsub-rest-proxy',
-  labelNames: ['channel'],
-});
+    name: 'pubsub_rest_proxy_receive_total',
+    help: 'Count of all messages recieved by pubsub-rest-proxy',
+    labelNames: ['channel']
+})
 
 const publishCount = new client.Counter({
-  name: 'pubsub_rest_proxy_publish_total',
-  help: 'Count of all messages published by pubsub-rest-proxy',
-  labelNames: ['state', 'channel'],
-});
+    name: 'pubsub_rest_proxy_publish_total',
+    help: 'Count of all messages published by pubsub-rest-proxy',
+    labelNames: ['state', 'channel']
+})
 
 const requestSummary = new client.Summary({
-  name: 'pubsub_rest_proxy_receive_to_publish_percentile',
-  help: 'Percentiles of time between receive and publish',
-  maxAgeSeconds: 600,
-  ageBuckets: 5,
-});
+    name: 'pubsub_rest_proxy_receive_to_publish_percentile',
+    help: 'Percentiles of time between receive and publish',
+    maxAgeSeconds: 600,
+    ageBuckets: 5
+})
 
 const messageCount = new client.Counter({
-  name: 'pubsub_rest_proxy_message_total',
-  help: 'Count of all individual messages published by pubsub-rest-proxy',
-  labelNames: ['state'],
-});
+    name: 'pubsub_rest_proxy_message_total',
+    help: 'Count of all individual messages published by pubsub-rest-proxy',
+    labelNames: ['state']
+})
 
-let app = express();
+let app = express()
 
 app.get('/metrics', (req, res) => {
-    res.end(client.register.metrics());
-});
+    res.end(client.register.metrics())
+})
 
-if( config.PROMETHEUS_EXPORTER ) {
-  console.log('Prometheus enabled on port: ' + config.PROMETHEUS_PORT);
-  app.listen(config.PROMETHEUS_PORT || 5000);
+if ( PROMETHEUS_EXPORTER ) {
+    logger.info('Prometheus enabled on port: ' + PROMETHEUS_PORT)
+    app.listen(PROMETHEUS_PORT || 5000)
 }
 
 module.exports = {
-  messageCount,
-  publishCount,
-  receiveCount,
-  requestSummary,
-};
+    messageCount,
+    publishCount,
+    receiveCount,
+    requestSummary
+}
